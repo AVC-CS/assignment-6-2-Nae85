e614afb5fe1c1440ba9433196a1cac63237b7c9c
Denae <hawkinsdenae@gmail.com>
2024-03-11 13:31:32 -0700

    6-2 assignment

--- main.hpp
+++ main.hpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 int getRandom(void);
-int getClosestMed(int, int, int);
+int getClosestMed(int num1, int num2, int num3);
 
 // complete two functions
 int getRandom(void)
@@ -34,14 +34,14 @@ int getClosestMed( num1, num2, num3)
   return rand() % 100;
 }
 {
-int getClosestMed(num1, num2, num3) {
+int getClosestMed(int num1, int num2, int num3) {
     int mean = (num1 + num2 + num3) / 3;
-    int diff1 = abs(num1 - mean);
-    int diff2 = abs(num2 - mean);
-    int diff3 = abs(num3 - mean);
+    int num1 = abs(num1 - mean);
+    int num2 = abs(num2 - mean);
+    int num3 = abs(num3 - mean);
     
     if (num1 <= num2 && num1 <= num3)
-        return rdnum1;
+        return num1;
     else if (num2 <= num1 && num2 <= num3)
         return num2;
     else
