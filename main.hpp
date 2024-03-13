27ed05a5c7ab2044bd3a98b10023d674b2fbdda6
Denae <hawkinsdenae@gmail.com>
2024-03-13 13:05:10 -0700

    6-2 assignment

--- main.hpp
+++ main.hpp
@@ -6,49 +6,28 @@
 #include <ctime>
 #include <cmath>
 using namespace std;
-
+int i;
 int getRandom(void);
 int getClosestMed(int num1, int num2, int num3);
 
 // complete two functions
-int getRandom(void)
-{
-         srand(time(NULL)); 
-        int num1 = getRandom();
-        int num2 = getRandom();
-        int num3 = getRandom();
-    
-        cout << "Random numbers: " << num1 << ", " << num2 << ", " << num3 << endl;
-    
-        int closestMed = getClosestMed(num1, num2, num3);
-    
-         cout << "Closest to the mean: " << closestMed << endl;
-    
-         return 0;
+int getRandom(void);
+int main() {
+        for (i=0; i<99; i++)
+                cout << getRandom() % 100 << endl;
 }
-        /***************************************************
-         * Code your program here
-         ***************************************************/
-{
-int getClosestMed( num1, num2, num3)
-  return rand() % 100;
-}
-{
-int getClosestMed(int num1, int num2, int num3) {
-    int mean = (num1 + num2 + num3) / 3;
-    int num1 = abs(num1 - mean);
-    int num2 = abs(num2 - mean);
-    int num3 = abs(num3 - mean);
-    
-    if (num1 <= num2 && num1 <= num3)
-        return num1;
-    else if (num2 <= num1 && num2 <= num3)
-        return num2;
-    else
-        return num3;
+ {
+        int getClosestMed();
+        {
+            int value = (num1 + num2 + num3) / 3;
+            cout << "the mean is " << value << endl;
+        }
 
+}
         /***************************************************
          * Code your program here
          ***************************************************/
-}
-}
+        /***************************************************
+         * Code your program here
+         ***************************************************/
+
